c397ba3118d27fef89dce6c96e84a7e6d31961e3
pshadikbasha <pshadikbasha97@gmail.com>
2021-09-15 15:25:28 +0530

    List rendering

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import Footer from "./components/Footer";
 import SideBar from "./components/SideBar";
 import MainPage from "./components/MainPage";
 import Greet from "./components/Greet";
+import Person from "./components/Person";
 class App extends React.Component {
+  state = {
+    data: [
+      { id: 1, name: "shadik", position: "developer" },
+      {
+        id: 2,
+        name: "koushik",
+        position: "Accenture",
+      },
+      {
+        id: 3,
+        name: "kalyan",
+        position: "Cognizant",
+      },
+    ],
+  };
   render() {
     return (
       <div>
-        <Greet></Greet>
+        {this.state.data.map((person) => {
+          return <Person name={person.name} position={person.position} />;
+        })}
+        {/* <Greet></Greet> */}
         {/* <Header></Header>
         <SideBar />
         <MainPage></MainPage>
---
+++ src/components/Person.jsx
@@ -0,0 +1,9 @@
+const Person = ({ name, position }) => {
+  return (
+    <div>
+      <h2>Name is {name}</h2>
+      <h4>Position is {position}</h4>
+    </div>
+  );
+};
+export default Person;
