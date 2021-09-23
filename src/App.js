10c21944ade7ff36348a7770cdf42cf90c53f186
pshadikbasha <pshadikbasha97@gmail.com>
2021-09-23 08:42:20 +0530

    event binding in react

--- src/App.js
+++ src/App.js
@@ -16,35 +16,51 @@ import Card from "./UiBrains/components/Card";
 import Navbar from "./UiBrains/components/Navbar/Navbar";
 import LandingPage from "./UiBrains/components/LandingPage/LandingPage";
 class App extends React.Component {
-  state = {
-    message: "Hello",
-  };
-  sayGoodMorning = (value) => {
-    this.setState({
-      message: value,
-    });
-  };
-  sayGoodAfternoon = () => {
-    this.setState({
-      message: "Hello GoodAfternoon",
-    });
-  };
-  sayGoodEvening = () => {
-    this.setState({
-      message: "Hello GoodEvening",
-    });
+  constructor(props) {
+    super(props);
+    console.log("the_logs_in_super", props);
+    this.state = {
+      product: {
+        price: 1,
+        name: "shadik",
+      },
+      name: "",
+    };
+  }
+  updatePrice = (value) => {
+    if (value === "+") {
+      this.setState({
+        product: {
+          ...this.state.product,
+          price: this.state.product.price + 1,
+        },
+      });
+    } else if (value === "-") {
+      this.setState({
+        product: {
+          ...this.state.product,
+          price:
+            this.state.product.price - 1 > 0 ? this.state.product.price - 1 : 1,
+        },
+      });
+    }
   };
   render() {
     return (
       <div>
         <Navbar></Navbar>
-        <h3>{this.state.message}</h3>
-        <button onClick={() => this.sayGoodMorning("goood morning")}>
-          Good Morning
-        </button>
-        <button onClick={this.sayGoodAfternoon}>Good Afternoon</button>
-        <button onClick={this.sayGoodEvening}>Good Evening</button>
-        <Card name="sadhik" />
+        <input
+          type="text"
+          name={this.state.name}
+          id=""
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <p>{this.state.name}</p>
+        <h3>name is{this.state.product.name}</h3>
+        <h3>price {this.state.product.price}</h3>
+        <button onClick={() => this.updatePrice("+")}>Add</button>
+        <button onClick={() => this.updatePrice("-")}>Sub</button>
       </div>
     );
   }
